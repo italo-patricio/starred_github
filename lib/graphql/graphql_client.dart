cd845e623b7cc03a70d96dd7be8a9419fc98dd14
Italo Patrício <italopatriciosouza@hotmail.com>
2021-05-06 07:29:59 -0300

    remove pasta nao utilizada

--- lib/graphql/graphql_client.dart
+++ lib/graphql/graphql_client.dart
@@ -5,7 +5,6 @@ GraphQLClient getGithubGraphQLClient() {
   final Link _link = HttpLink(
     'https://api.github.com/graphql',
     defaultHeaders: {
-      // ignore: undefined_identifier
       'Authorization': 'Bearer $accessTokenGithub',
     },
   );
